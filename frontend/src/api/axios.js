39eae8f8572c0c71bf61e5a906a872de82a3cc41
pranav <pranavrudraraju6@gmail.com>
2026-01-05 16:57:11 +0530

    production test 2

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
+console.log("🔥 API BASE URL:", import.meta.env.VITE_API_URL);
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
